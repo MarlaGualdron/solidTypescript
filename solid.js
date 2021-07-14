a18c90ae6e83a3fa373e15696b51f25256aefc7c
ihsa-marla <ma.gualdron@gmail.com>
2021-07-14 10:38:36 -0300

    Se completa el script teniendo en cuenta Psi y Pis

--- solid.js
+++ solid.js
@@ -19,7 +19,7 @@ var __extends = (this && this.__extends) || (function () {
 var main1 = function () {
     var Coche = /** @class */ (function () {
         /*   al constructor generalmente le paso los atributos
-          si no se los paso màs adelante hago un mètodo que permita modificarlo  */
+          si no se los paso mÃ s adelante hago un mÃĻtodo que permita modificarlo  */
         function Coche(marca) {
             this.marca = marca;
         }
@@ -35,10 +35,11 @@ var main1 = function () {
 };
 // O: Principio abierto/cerrado
 /* Sea mas facil de extender sin necesidad de generar
-muchas implicancias de corregir en el còdigo (Herencia de clases
+muchas implicancias de corregir en el cÃēdigo (Herencia de clases
   para solo poner una propiedad de la clase abstracta) */
 /* Cerrado porque se quiere indicar que el
 precio es una propiedad de cada uno de los autos */
+// Ejemplo con error
 var mainPacError = function () {
     var Coche = /** @class */ (function () {
         function Coche(marca) {
@@ -70,6 +71,7 @@ var mainPacError = function () {
     console.log("main Principio Abierto Cerrado Error");
     main();
 };
+// Ejemplo corregido
 var mainPac = function () {
     var Coche = /** @class */ (function () {
         function Coche() {
@@ -112,4 +114,284 @@ var mainPac = function () {
     console.log("main Principio Abierto Cerrado");
     main();
 };
-mainPac();
+// L: Principio de substitucion de Liskov
+/* Una subclase debe poder ser sustituible por su subclase. De no ser asi no se
+* cumple el principio */
+var mainPSLiskovError = function () {
+    var Coche = /** @class */ (function () {
+        function Coche() {
+        }
+        return Coche;
+    }());
+    var Renault = /** @class */ (function (_super) {
+        __extends(Renault, _super);
+        function Renault() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        Renault.prototype.precioMedioCoche = function () {
+            return 18000;
+        };
+        return Renault;
+    }(Coche));
+    var Audi = /** @class */ (function (_super) {
+        __extends(Audi, _super);
+        function Audi() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        Audi.prototype.precioMedioCoche = function () {
+            return 25000;
+        };
+        return Audi;
+    }(Coche));
+    var Mercedes = /** @class */ (function (_super) {
+        __extends(Mercedes, _super);
+        function Mercedes() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        Mercedes.prototype.precioMedioCoche = function () {
+            return 25000;
+        };
+        return Mercedes;
+    }(Coche));
+    var numAsientosRenault = function () {
+        return 4;
+    };
+    var numAsientosAudi = function () {
+        return 2;
+    };
+    var numAsientosMercedes = function () {
+        return 4;
+    };
+    var imprimirAsientos = function (arrayCoches) {
+        for (var _i = 0, arrayCoches_3 = arrayCoches; _i < arrayCoches_3.length; _i++) {
+            var coche = arrayCoches_3[_i];
+            if (coche instanceof Renault) {
+                console.log(numAsientosRenault());
+            }
+            if (coche instanceof Audi) {
+                console.log(numAsientosRenault());
+            }
+            if (coche instanceof Mercedes) {
+                console.log(numAsientosRenault());
+            }
+        }
+    };
+    var main = function () {
+        var arrayCoches = [
+            new Renault(),
+            new Audi(),
+            new Mercedes()
+        ];
+        imprimirAsientos(arrayCoches);
+    };
+    console.log("main Principio de substitucion de Liskov con Error");
+    main();
+};
+var mainPSLiskov = function () {
+    var Coche = /** @class */ (function () {
+        function Coche() {
+        }
+        return Coche;
+    }());
+    var Renault = /** @class */ (function (_super) {
+        __extends(Renault, _super);
+        function Renault() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        Renault.prototype.precioMedioCoche = function () {
+            return 18000;
+        };
+        Renault.prototype.numAsientos = function () {
+            return 4;
+        };
+        return Renault;
+    }(Coche));
+    var Audi = /** @class */ (function (_super) {
+        __extends(Audi, _super);
+        function Audi() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        Audi.prototype.precioMedioCoche = function () {
+            return 25000;
+        };
+        Audi.prototype.numAsientos = function () {
+            return 2;
+        };
+        return Audi;
+    }(Coche));
+    var Mercedes = /** @class */ (function (_super) {
+        __extends(Mercedes, _super);
+        function Mercedes() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        Mercedes.prototype.precioMedioCoche = function () {
+            return 25000;
+        };
+        Mercedes.prototype.numAsientos = function () {
+            return 4;
+        };
+        return Mercedes;
+    }(Coche));
+    var Ford = /** @class */ (function (_super) {
+        __extends(Ford, _super);
+        function Ford() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        Ford.prototype.precioMedioCoche = function () {
+            return 25000;
+        };
+        Ford.prototype.numAsientos = function () {
+            return 4;
+        };
+        return Ford;
+    }(Coche));
+    var imprimirAsientos = function (arrayCoches) {
+        for (var _i = 0, arrayCoches_4 = arrayCoches; _i < arrayCoches_4.length; _i++) {
+            var coche = arrayCoches_4[_i];
+            console.log(coche.numAsientos());
+        }
+    };
+    var main = function () {
+        var arrayCoches = [
+            new Renault(),
+            new Audi(),
+            new Mercedes(),
+            new Ford()
+        ];
+        imprimirAsientos(arrayCoches);
+    };
+    console.log("main Principio de substitucion de Liskov con Error");
+    main();
+};
+// I: Principio de segregacion de interfaz
+/* No se debe depender de interfaces que no se usan, se tiene que crear las propias en caso de no existir */
+var mainPSIError = function () {
+    var Loro = /** @class */ (function () {
+        function Loro() {
+        }
+        Loro.prototype.volar = function () {
+        };
+        Loro.prototype.comer = function () {
+        };
+        Loro.prototype.nadar = function () {
+        };
+        return Loro;
+    }());
+    var Tucan = /** @class */ (function () {
+        function Tucan() {
+        }
+        Tucan.prototype.volar = function () {
+        };
+        Tucan.prototype.comer = function () {
+        };
+        Tucan.prototype.nadar = function () {
+        };
+        return Tucan;
+    }());
+    var Pinguino = /** @class */ (function () {
+        function Pinguino() {
+        }
+        Pinguino.prototype.volar = function () {
+        };
+        Pinguino.prototype.comer = function () {
+        };
+        Pinguino.prototype.nadar = function () {
+        };
+        return Pinguino;
+    }());
+};
+var mainPSI = function () {
+    var Loro = /** @class */ (function () {
+        function Loro() {
+        }
+        Loro.prototype.comer = function () {
+        };
+        Loro.prototype.volar = function () {
+        };
+        return Loro;
+    }());
+    var Pinguino = /** @class */ (function () {
+        function Pinguino() {
+        }
+        Pinguino.prototype.comer = function () {
+        };
+        Pinguino.prototype.nadar = function () {
+        };
+        return Pinguino;
+    }());
+};
+// D: Principio de inversion de dependencias.
+/* Cuando se usa una clase, y es necesario tener una dependencia, se debe usar
+* la clase abstracta en lugar de la concreta. */
+var mainPIDError = function () {
+    var Dato = /** @class */ (function () {
+        function Dato() {
+        }
+        return Dato;
+    }());
+    var DatabaseService = /** @class */ (function () {
+        function DatabaseService() {
+        }
+        DatabaseService.prototype.getDatos = function () {
+        };
+        return DatabaseService;
+    }());
+    var AccesoADatos = /** @class */ (function () {
+        function AccesoADatos(databaseService) {
+            this.databaseService = databaseService;
+        }
+        AccesoADatos.prototype.getDatos = function () {
+            this.databaseService.getDatos();
+            //...
+            return new Dato();
+        };
+        return AccesoADatos;
+    }());
+};
+var mainPID = function () {
+    var Dato = /** @class */ (function () {
+        function Dato() {
+        }
+        return Dato;
+    }());
+    var DatabaseService = /** @class */ (function () {
+        function DatabaseService() {
+        }
+        DatabaseService.prototype.getDatos = function () {
+            return new Dato();
+        };
+        DatabaseService.prototype.setDatos = function () {
+        };
+        return DatabaseService;
+    }());
+    var APIService = /** @class */ (function () {
+        function APIService() {
+        }
+        APIService.prototype.getDatos = function () {
+            return new Dato();
+        };
+        APIService.prototype.setDatos = function () {
+        };
+        return APIService;
+    }());
+    var AccesoADatos = /** @class */ (function () {
+        function AccesoADatos(conexion) {
+            this.conexion = conexion;
+        }
+        AccesoADatos.prototype.getDatos = function () {
+            this.conexion.getDatos();
+            //...
+            return new Dato();
+        };
+        return AccesoADatos;
+    }());
+};
+// main1()
+// mainPacError()
+// mainPac()
+// mainPSLiskovError()
+// mainPSLiskov()
+// mainPSIError()
+// mainPSI()
+// mainPIDError()
+// mainPID()
--- solid.ts
+++ solid.ts
@@ -7,7 +7,7 @@ const main1 = function () {
     marca: string;
     patente: number;
   /*   al constructor generalmente le paso los atributos
-    si no se los paso màs adelante hago un mètodo que permita modificarlo  */
+    si no se los paso mÃ s adelante hago un mÃĻtodo que permita modificarlo  */
     constructor (marca:string){
       this.marca = marca;
     }
@@ -27,11 +27,12 @@ const main1 = function () {
 // O: Principio abierto/cerrado
 
 /* Sea mas facil de extender sin necesidad de generar
-muchas implicancias de corregir en el còdigo (Herencia de clases
+muchas implicancias de corregir en el cÃēdigo (Herencia de clases
   para solo poner una propiedad de la clase abstracta) */
 /* Cerrado porque se quiere indicar que el 
 precio es una propiedad de cada uno de los autos */
 
+// Ejemplo con error
 const mainPacError = function () {
   class Coche {
     marca: string;
@@ -65,10 +66,11 @@ const mainPacError = function () {
     imprimirPrecio(arrayCoches)
   }
 
-  console.log("main Principio Abierto Cerrado Error")
-  main()
+  console.log("main Principio Abierto Cerrado Error");
+  main();
 }
 
+// Ejemplo corregido
 const mainPac = function () {
   abstract class Coche {
     marca: string
@@ -101,9 +103,298 @@ const mainPac = function () {
     ]
     imprimirPrecio(arrayCoches);
   }
-  console.log("main Principio Abierto Cerrado")
+  console.log("main Principio Abierto Cerrado");
+  main();
+}
+
+
+
+// L: Principio de substitucion de Liskov
+
+/* Una subclase debe poder ser sustituible por su subclase. De no ser asi no se
+* cumple el principio */
+const mainPSLiskovError = function() {
+  abstract class Coche {
+    marca: string
+    precio:number
+    abstract precioMedioCoche():number
+  }
+
+  class Renault extends Coche {
+    precioMedioCoche():number{
+      return 18000
+    }
+  }
+
+  class Audi extends Coche {
+    precioMedioCoche():number{
+      return 25000
+    }
+  }
+
+  class Mercedes extends Coche {
+    precioMedioCoche():number{
+      return 25000
+    }
+  }
+
+  const numAsientosRenault = function() {
+    return 4
+  }
+  const numAsientosAudi = function() {
+    return 2
+  }
+  const numAsientosMercedes = function() {
+    return 4
+  }
+  const imprimirAsientos = function (arrayCoches: Coche[]) {
+    for (let coche of arrayCoches) {
+      if (coche instanceof Renault){
+        console.log(numAsientosRenault())
+      }
+      if (coche instanceof Audi){
+        console.log(numAsientosRenault())
+      }
+      if (coche instanceof Mercedes){
+        console.log(numAsientosRenault())
+      }
+    }
+  }
+
+  const main = function () {
+    const arrayCoches: Coche[] = [
+      new Renault(),
+      new Audi(),
+      new Mercedes()
+    ]
+    imprimirAsientos(arrayCoches);
+  }
+  console.log("main Principio de substitucion de Liskov con Error")
   main()
 }
 
 
-mainPac()
+const mainPSLiskov = function() {
+  abstract class Coche {
+    marca: string
+    precio:number
+    abstract precioMedioCoche():number
+    abstract numAsientos():number
+  }
+
+  class Renault extends Coche {
+    precioMedioCoche():number{
+      return 18000
+    }
+    numAsientos():number{
+      return 4
+    }
+  }
+
+  class Audi extends Coche {
+    precioMedioCoche():number{
+      return 25000
+    }
+    numAsientos():number{
+      return 2
+    }
+  }
+
+  class Mercedes extends Coche {
+    precioMedioCoche():number{
+      return 25000
+    }
+    numAsientos():number{
+      return 4
+    }
+  }
+
+  class Ford extends Coche {
+    precioMedioCoche():number{
+      return 25000
+    }
+    numAsientos():number{
+      return 4
+    }
+  }
+
+  const imprimirAsientos = function (arrayCoches: Coche[]) {
+    for (let coche of arrayCoches) {
+      console.log(coche.numAsientos())
+    }
+  }
+
+  const main = function () {
+    const arrayCoches: Coche[] = [
+      new Renault(),
+      new Audi(),
+      new Mercedes(),
+      new Ford()
+    ]
+    imprimirAsientos(arrayCoches);
+  }
+  console.log("main Principio de substitucion de Liskov con Error")
+  main()
+}
+
+// I: Principio de segregacion de interfaz
+
+/* No se debe depender de interfaces que no se usan, se tiene que crear las propias en caso de no existir */
+
+const mainPSIError = function() {
+  interface IAve{
+    volar():void
+    comer():void
+    nadar():void
+  }
+  class Loro implements IAve{
+    volar():void{
+
+    }
+    comer():void{
+
+    }
+    nadar():void{
+
+    }
+  }
+  class Tucan implements IAve{
+    volar():void{
+
+    }
+    comer():void{
+
+    }
+    nadar():void{
+
+    }
+  }
+  class Pinguino implements IAve{
+    volar():void{
+
+    }
+    comer():void{
+
+    }
+    nadar():void{
+
+    }
+  }
+}
+
+const mainPSI = function(){
+  interface IAve{
+    comer():void
+  }
+  interface IAveVoladora{
+    volar():void
+  }
+  interface IAveNadadora{
+    nadar():void
+  }
+
+  class Loro implements IAve,IAveVoladora{
+    comer():void{
+
+    }
+    volar():void{
+
+    }
+  }
+
+  class Pinguino implements IAve,IAveNadadora{
+    comer():void{
+
+    }
+    nadar():void{
+
+    }
+  }
+
+}
+
+// D: Principio de inversion de dependencias.
+
+/* Cuando se usa una clase, y es necesario tener una dependencia, se debe usar
+* la clase abstracta en lugar de la concreta. */
+
+const mainPIDError = function() {
+
+  class Dato {
+
+  }
+
+  class DatabaseService{
+    getDatos(): void{
+
+    }
+  }
+
+  class AccesoADatos{
+    databaseService:DatabaseService
+
+    constructor(databaseService:DatabaseService){
+      this.databaseService = databaseService
+    }
+
+    getDatos():Dato {
+      this.databaseService.getDatos()
+      //...
+      return new Dato()
+    }
+  }
+}
+
+const mainPID = function() {
+
+  class Dato {
+
+  }
+
+  interface Conexion{
+    getDatos():Dato
+    setDatos():void
+  }
+
+  class DatabaseService implements Conexion{
+    getDatos(): Dato {
+      return new Dato()
+    }
+    setDatos():void {
+
+    }
+  }
+
+  class APIService implements Conexion{
+    getDatos(): Dato {
+      return new Dato()
+    }
+    setDatos():void {
+
+    }
+  }
+
+  class AccesoADatos{
+    conexion:Conexion
+
+    constructor(conexion:Conexion){
+      this.conexion = conexion
+    }
+
+    getDatos():Dato {
+      this.conexion.getDatos()
+      //...
+      return new Dato()
+    }
+  }
+}
+
+// main1()
+// mainPacError()
+// mainPac()
+// mainPSLiskovError()
+// mainPSLiskov()
+// mainPSIError()
+// mainPSI()
+// mainPIDError()
+// mainPID()
